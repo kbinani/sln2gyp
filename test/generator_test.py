fe5dccac11bd97e275e2b1724b032c030535edcb
kbinani <kbinani.bt@gmail.com>
2014-06-20 19:20:07 +0900

    Simplify the test case for sln2gyp.Generator

--- test/generator_test.py
+++ test/generator_test.py
@@ -5,63 +5,6 @@ import sln2gyp
 
 class GeneratorTest(unittest.TestCase):
 	def test_generate_gyp(self):
-		sln_file = 'test/fixtures/simple2012/Win32Project1/Win32Project1.sln'
-		abs_sln_file = os.path.abspath(sln_file)
-		abs_sln_dir = os.path.dirname(abs_sln_file)
-
-		solution = sln2gyp.Solution(abs_sln_file)
-		generator = sln2gyp.Generator()
-		generator.generate_gyp(solution)
-
-		# root gyp file, correspoinding to 'Win32Project1.sln'
-		expected_root_gyp_path = os.path.join(abs_sln_dir, 'Win32Project1.gyp')
-		self.assertTrue(os.path.exists(expected_root_gyp_path))
-
-		expected_contents = {
-			'targets': [
-				{
-					'target_name': 'All',
-					'type': 'none',
-					'dependencies': [
-						'Win32Project1/Win32Project1.gyp:Win32Project1',
-					]
-				}
-			]
-		}
-		actual_contents = json.loads(open(expected_root_gyp_path).read())
-		self.assertEqual(expected_contents, actual_contents)
-
-		# dependent gypi file, correspoinding to 'Win32Project1/Win32Project1.vcxproj'
-		expected_depend_gyp_path = os.path.join(abs_sln_dir, 'Win32Project1/Win32Project1.gyp')
-		self.assertTrue(os.path.exists(expected_depend_gyp_path))
-
-		expected_contents = {
-			'targets': [
-				{
-					'target_name': 'Win32Project1',
-					'type': 'executable',
-					'sources': [
-						'stdafx.cpp',
-						'Win32Project1.cpp',
-						'Resource.h',
-						'stdafx.h',
-						'targetver.h',
-						'Win32Project1.h',
-					],
-					'configurations': {
-						'Debug': {
-						},
-						'Release': {
-						},
-					}
-				}
-			]
-		}
-		actual_contents = json.loads(open(expected_depend_gyp_path).read())
-		self.assertEqual(expected_contents, actual_contents)
-
-class GeneratorWithDependenciesTestCase(unittest.TestCase):
-	def test_generate_gyp(self):
 		sln_file = 'test/fixtures/vs2012/dependency/Win32Project1/Win32Project1.sln'
 		abs_sln_file = os.path.abspath(sln_file)
 		abs_sln_dir = os.path.dirname(abs_sln_file)
@@ -90,33 +33,62 @@ class GeneratorWithDependenciesTestCase(unittest.TestCase):
 		self.assertEqual(expected_contents, actual_contents)
 
 		# dependent gypi file, correspoinding to 'Win32Project1/Win32Project1.vcxproj'
-		expected_depend_gyp_path = os.path.join(abs_sln_dir, 'Win32Project1/Win32Project1.gyp')
-		self.assertTrue(os.path.exists(expected_depend_gyp_path))
+		def assert_Win32Project_proj():
+			expected_depend_gyp_path = os.path.join(abs_sln_dir, 'Win32Project1/Win32Project1.gyp')
+			self.assertTrue(os.path.exists(expected_depend_gyp_path))
 
-		expected_contents = {
-			'targets': [
-				{
-					'target_name': 'Win32Project1',
-					'type': 'executable',
-					'dependencies': [
-						'../depend/depend.gyp:depend',
-					],
-					'sources': [
-						'stdafx.cpp',
-						'Win32Project1.cpp',
-						'Resource.h',
-						'stdafx.h',
-						'targetver.h',
-						'Win32Project1.h',
-					],
-					'configurations': {
-						'Debug': {
-						},
-						'Release': {
-						},
+			expected_contents = {
+				'targets': [
+					{
+						'target_name': 'Win32Project1',
+						'type': 'executable',
+						'dependencies': [
+							'../depend/depend.gyp:depend',
+						],
+						'sources': [
+							'stdafx.cpp',
+							'Win32Project1.cpp',
+							'Resource.h',
+							'stdafx.h',
+							'targetver.h',
+							'Win32Project1.h',
+						],
+						'configurations': {
+							'Debug': {
+							},
+							'Release': {
+							},
+						}
 					}
-				}
-			]
-		}
-		actual_contents = json.loads(open(expected_depend_gyp_path).read())
-		self.assertEqual(expected_contents, actual_contents)
+				]
+			}
+			actual_contents = json.loads(open(expected_depend_gyp_path).read())
+			self.assertEqual(expected_contents, actual_contents)
+		assert_Win32Project_proj()
+
+		def assert_depend_proj():
+			expected_depend_gyp_path = os.path.join(abs_sln_dir, 'depend/depend.gyp')
+			self.assertTrue(os.path.exists(expected_depend_gyp_path))
+
+			expected_contents = {
+				'targets': [
+					{
+						'target_name': 'depend',
+						'type': 'static_library',
+						'sources': [
+							'stdafx.cpp',
+							'stdafx.h',
+							'targetver.h',
+						],
+						'configurations': {
+							'Debug': {
+							},
+							'Release': {
+							},
+						}
+					}
+				]
+			}
+			actual_contents = json.loads(open(expected_depend_gyp_path).read())
+			self.assertEqual(expected_contents, actual_contents)
+		assert_depend_proj()
